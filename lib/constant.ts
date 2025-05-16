ed2b62d436e688cc71ec85943a70fb70b991a876
Ayush <ayushkansal303@gmail.com>
2025-05-16 22:48:01 +0530

    fixed some

---
+++ lib/constant.ts
@@ -0,0 +1,55 @@
+const layoutMetadata = {
+    title: 'NFun - Turn NFTs into Viral Memes Instantly',
+    description:
+        'Create and mint viral NFT memes in seconds with NFun! Customize styles, add captions, and share your Squid Game-inspired creations.',
+    keywords: [
+        'NFT memes',
+        'meme generator',
+        'NFun',
+        'viral memes',
+        'Squid Game memes',
+        'NFT creator',
+        'meme minting',
+        'X memes',
+    ],
+    openGraph: {
+        title: 'NFun - Instant Viral NFT Memes',
+        description:
+            'Transform NFTs into memes with captions and Squid Game-inspired styles. Mint or share in seconds with NFun. Follow @NFToodleHQ on X!',
+        url: 'https://NFun.ayverse.me',
+        siteName: 'NFun',
+        images: [
+            {
+                url: 'https://pbs.twimg.com/profile_images/1903891969476059136/ovNOfG-d_400x400.jpg',
+                width: 400,
+                height: 400,
+                alt: 'NFun Viral NFT Meme Creator',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'NFun - Create Viral NFT Memes',
+        description:
+            'Turn NFTs into memes in seconds with NFun! Customize, mint, or share with our Squid Game-themed generator. Follow @NFToodleHQ on X!',
+        images: [
+            'https://pbs.twimg.com/profile_images/1903891969476059136/ovNOfG-d_400x400.jpg',
+        ],
+        site: '@NFToodleHQ',
+    },
+    alternates: {
+        canonical: 'https://NFun.ayverse.me',
+    },
+    robots: {
+        index: true,
+        follow: true,
+    },
+}
+
+
+
+export {
+    layoutMetadata
+}
